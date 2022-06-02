899999640a731e57b3ac2c41930601ad50bfafc4
dudskrla <dudskrla09@gmail.com>
2022-06-02 17:32:22 +0000

    [Feat] Upload word2line

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ import word2line
 
 app = FastAPI()
 
-with open('./api_info.json', 'rb') as f:
+with open("./api_info.json", "rb") as f:
     api_infos = json.load(f)
 
 
@@ -18,11 +18,61 @@ def read_imagefile(file) -> BytesIO:
 
 @app.post("/ocr")
 async def classfication(file: UploadFile = File(...)):
-    #files = read_imagefile(await file.read())
-    files = {'file': file.file}
-    ocr = requests.post(api_infos['api_url'], headers=api_infos['headers'], files=files).json()
-    if ocr.get('ocr', 0):
-        result = word2line.word2line(ocr)
-    else:
-        result = []
-    return {'result': result}
+    files = {"file": file.file}
+    result = requests.post(
+        api_infos["api_url"], headers=api_infos["headers"], files=files
+    ).json()
+    result = word2line.word2line(result)
+    return {"result": result}
+import json
+from io import BytesIO
+
+import requests
+from fastapi import FastAPI, UploadFile, File
+
+import word2line
+
+app = FastAPI()
+
+with open("./api_info.json", "rb") as f:
+    api_infos = json.load(f)
+
+
+def read_imagefile(file) -> BytesIO:
+    return BytesIO(file)
+
+
+@app.post("/ocr")
+async def classfication(file: UploadFile = File(...)):
+    files = {"file": file.file}
+    result = requests.post(
+        api_infos["api_url"], headers=api_infos["headers"], files=files
+    ).json()
+    result = word2line.word2line(result)
+    return {"result": result}
+import json
+from io import BytesIO
+
+import requests
+from fastapi import FastAPI, UploadFile, File
+
+import word2line
+
+app = FastAPI()
+
+with open("./api_info.json", "rb") as f:
+    api_infos = json.load(f)
+
+
+def read_imagefile(file) -> BytesIO:
+    return BytesIO(file)
+
+
+@app.post("/ocr")
+async def classfication(file: UploadFile = File(...)):
+    files = {"file": file.file}
+    result = requests.post(
+        api_infos["api_url"], headers=api_infos["headers"], files=files
+    ).json()
+    result = word2line.word2line(result)
+    return {"result": result}
