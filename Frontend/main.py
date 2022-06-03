766e781ec30f1853dcc0e5a98c07cd4686e10345
Heruing <s_1012@naver.com>
2022-06-03 05:02:20 +0000

    [Fix] FE interface

--- Frontend/main.py
+++ Frontend/main.py
@@ -1,15 +1,16 @@
 import sys
+from turtle import onclick
 
 import requests
 import streamlit as st
 from PIL import Image, ImageDraw
 from streamlit import cli as stcli
+from io import BytesIO
+import base64
 
-from convert import converter
-import io
 
 def load_image(image_file):
-    img = Image.open(image_file)
+    img = Image.open(BytesIO(image_file))
     return img
 
 
@@ -19,17 +20,20 @@ def draw_rectengle(image, point1, point2):
     return image
 
 def image_to_byte_array(image: Image) -> bytes:
-    imgByteArr = io.BytesIO()
+    imgByteArr = BytesIO()
     image.save(imgByteArr, format="JPEG")
-    imgByteArr = imgByteArr.getvalue()
     return imgByteArr
 
-def to_ocr(data):
-    files = {"file": data.getvalue()}
-    result = requests.post("http://127.0.0.1:8000/ocr", files=files).json()
-    ocr_image = load_image(data)
-    if result['result']:
-        for i in result['result']['ocr']['word']:
+
+def to_ocr(byteImage, threshold, invert):
+    param = {"threshold":threshold, "invert":invert}
+    files = {"file": byteImage.getvalue()}
+    result = requests.post(f"http://127.0.0.1:8000/ocr/", params=param, files=files).json()
+    image = base64.b64decode(result['image'])
+    ocr_image = load_image(image)
+    st.image(ocr_image)
+    if result['ocr']:
+        for i in result['ocr']['word']:
             f, _, s, _ = i['points']
             draw_rectengle(ocr_image, f, s)
         st.write("Output")
@@ -38,29 +42,27 @@ def to_ocr(data):
         st.warning("Can not find any Data")
 
 
+
 def main():
     st.sidebar.header("Select service")
-    name = st.sidebar.selectbox("Service", ["ImageNet"])
+    name = st.sidebar.selectbox("Service", ["Auto Fix", "Fix Input"])
     st.title("OCR API Test")
 
-    with st.form('my_form'):
-        image_file = st.file_uploader("Choose an image", type=["png", "jpg", "jpeg"])
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            to_ocr(image_file)
-    self_fix = st.checkbox("Auto Fix", value=True)
-    col1, col2 = st.columns(2)
+    image_file = st.file_uploader("Choose an image", type=["png", "jpg", "jpeg"])
     if image_file:
-        with col1:
-            bytes_data = image_file.getvalue()
-            st.write("clear the check box if you want directly adjust")
-            threshold = "auto"
-            invert = False
-            
-            if self_fix:
-                st.warning("Automatic revision is appied on data.\nWe recommend uncheck above checkbox and self revision for fine data")
-            else:
-                img = load_image(image_file)
+        if name == "Auto Fix":
+            st.image(image_file)
+            submitted = st.button("Submit")
+            if submitted:
+                to_ocr(image_file, -1, 0)
+                st.warning("If Not satisfied with Results: Use sidebar menu 'Fix Input' to fix input image by yourself")
+        if name == "Fix Input":
+            col1, col2 = st.columns(2)
+            with col1:
+                bytes_data = image_file.getvalue()
+                st.write("clear the check box if you want directly adjust")
+                threshold = -1
+                img = load_image(bytes_data)
                 st.write('Rotate Angle')
                 angle_fix = st.slider('Fix Angle', value=0, min_value = -180, max_value=180)
                 angle_fixed = img.rotate(-angle_fix)
@@ -76,24 +78,19 @@ def main():
                     img = angle_fixed.crop((start_x, start_y, end_x, end_y))
                     cropped.paste(img, (start_x, start_y))
                     cropped = draw_rectengle(cropped, (start_x, start_y), (end_x, end_y))
-
                 bytes_data = image_to_byte_array(img)
-
-                invert = st.checkbox("Invert: Check if image darker than BG.")
+                invert = 1 if st.checkbox("Invert: Check if image darker than BG.") else 0
                 threshold = st.slider('Change Threshold value', value=180, min_value=0, max_value=255)
-
-            
-        with col2:
-            if not self_fix:
+            resubmit = 0
+            with col2:
                 with st.form('self_fix'):
                     st.image(cropped)
                     resubmit = st.form_submit_button("Submit")
                     if resubmit:
-                        value = (threshold, invert)
-                        converted = converter(bytes_data, value)
                         st.write("Submitted Image.")
-                        st.image(converted)
-                        to_ocr(converted)
+                        st.image(bytes_data)
+            if resubmit:
+                to_ocr(bytes_data, threshold, invert)
 
     
 if __name__ == '__main__':
