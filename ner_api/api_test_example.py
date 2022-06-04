f140590b5c97406f3507a8d9c174244d4c6955e7
Su-minn <smsj68@gmail.com>
2022-06-04 15:08:25 +0000

    [Add] Add Dockerfile

--- ner_api/api_test_example.py
+++ ner_api/api_test_example.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 
-URL = "http://0.0.0.0:8000/ner"
+URL = "http://0.0.0.0:30005/ner"
 datas = {'text':
         ['김민수 삼성전자 tom 서울특별시 박민수 영등포구 123-45 010-1234-5687', '경기도 수원시 장안구 010-5456-5654 이지연 LG전자']
 }
