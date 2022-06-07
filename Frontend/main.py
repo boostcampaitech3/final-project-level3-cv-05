80ce30d2ff10c5bc1b527a2f57de0062da39e855
KyubumShin <kbum0617@naver.com>
2022-06-07 11:55:26 +0900

    [Feat] Update main.py for demo page

--- Frontend/main.py
+++ Frontend/main.py
@@ -5,16 +5,33 @@ import streamlit as st
 from PIL import Image
 from PIL import ImageDraw
 from streamlit import cli as stcli
+import seaborn as sns
+import pandas as pd
 
+cat = {
+    0: "UNKNOWN",
+    1: "name",
+    2: "phone",
+    3: "email",
+    4: "position",
+    5: "company",
+    6: "department",
+    7: "address",
+    8: "site",
+    9: "account",
+    10: "wise",
+}
+colors = sns.color_palette('bright', 11)
+palette = [tuple([int(i*255) for i in color]) for color in colors]
 
 def load_image(image_file):
     img = Image.open(image_file)
     return img
 
 
-def draw_rectengle(image, point1, point2):
+def draw_rectengle(image, point1, point2, color_cat):
     draw = ImageDraw.Draw(image)
-    draw.rectangle((point1[0], point1[1], point2[0], point2[1]), outline=(0, 0, 255), width=1)
+    draw.rectangle((point1[0], point1[1], point2[0], point2[1]), outline=palette[color_cat], width=3)
     return image
 
 
@@ -30,11 +47,16 @@ def main():
             files = {"file": image_file.getvalue()}
             st.write("Result")
             result = requests.post("http://127.0.0.1:8000/ocr", files=files).json()
-            img = load_image(image_file)
+            img1 = load_image(image_file)
+            text, category = [], []
             for i in result['result']['ocr']['word']:
                 f, _, s, _ = i['points']
-                draw_rectengle(img, f, s)
-            st.image(img)
+                text.append(i['text'])
+                category.append(cat[i["total_cat"]])
+                draw_rectengle(img1, f, s, i['total_cat'])
+            st.image(img1)
+            df = pd.DataFrame.from_dict({"text": text, "category": category})
+            st.dataframe(data=df, width=600, height=500)
 
 
 if __name__ == '__main__':
