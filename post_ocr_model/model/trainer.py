5232d16d0ce7e002a366132c71a51f8ede7e672f
sw930718 <in5451@naver.com>
2022-06-07 09:29:09 +0000

    [Feat] Add logger, callbacks

--- post_ocr_model/model/trainer.py
+++ post_ocr_model/model/trainer.py
@@ -39,8 +39,22 @@ def main():
     cfg = config(args)
     datamodule = PostOCRDataLoader(cfg, **cfg.Dataset)
     model = PostOCRLearner(cfg)
-    logger = None
-    callbacks = None
+
+    logger = WandbLogger(project="project name", entity="wandb id", name='running name')
+    
+    """ 
+    [ Checkpoint Callback ]
+
+    filepath: 체크포인트 저장위치와 이름 형식을 지정
+    verbose: 체크포인트 저장 결과를 출력
+    save_last: 마지막 체크포인트를 저장
+    save_top_k: 최대 몇 개의 체크포인트를 저장할지 지정(save_last에 의해 저장되는 체크포인트는 제외)
+    monitor: 어떤 metric을 기준으로 체크포인트를 저장할지 지정
+    ['train_loss', 'train_accuracy', 'val_loss', 'val_accuracy', 'epoch', 'step']
+    mode: 지정한 metric의 어떤 기준(ex. min, max)으로 체크포인트를 저장할지 지정
+    """   
+    callbacks = pl.callbacks.ModelCheckpoint(filename='{epoch}_{val_accuracy:.4f}',save_top_k=2, monitor='val_accuracy', mode='max')
+
     trainer = pl.Trainer(
         logger=logger,
         callbacks=callbacks,
