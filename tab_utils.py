a98000ca9baff18c4029c185a9a0d8e02f3c58a3
sw930718 <in5451@naver.com>
2022-06-08 02:52:51 +0000

    [Feat] Update tab_utils

--- tab_utils.py
+++ tab_utils.py
@@ -1,6 +1,5 @@
 """
 Feature Engineerin을 위한 유틸리티 함수 들을 모아놓은 모듈
-
 Functions:
     _read_json(path: str): 
 """
@@ -11,14 +10,14 @@ import pandas as pd
 import numpy as np
 
 def _read_json(path: str) -> dict:
-    with open(path, 'r', encoding = 'utf-8') as f:
+    with open(path, 'r', encoding='utf-8') as f:
         info = json.load(f)
     return info
 
 
 def _json_to_dataframe(info: dict, pre_features: List[str]) -> pd.DataFrame:
     image_len = len(info['images'])
-    df_result = pd.DataFrame(columns = pre_features)
+    df_result = pd.DataFrame(columns=pre_features)
 
     for idx in range(image_len):
         file_name = info['images'][idx]['file']
@@ -26,7 +25,7 @@ def _json_to_dataframe(info: dict, pre_features: List[str]) -> pd.DataFrame:
         image_height = info['images'][idx]['height']
         words = info['annotations'][idx]['ocr']['word']
         temp_dict = {}
-        
+
         for word in words:
             category_id = word['category_id']
             points = word['points']
@@ -34,25 +33,24 @@ def _json_to_dataframe(info: dict, pre_features: List[str]) -> pd.DataFrame:
             orientation = word['orientation']
             text = word['text']
             temp_dict = {
-                'file_name' : file_name,
-                'image_width' : image_width,
-                'image_height' : image_height,
-                'category_id' : category_id,
-                'points' : [points],
-                'point_1' : [point_1],
-                'point_2' : [point_2],
-                'point_3' : [point_3],
-                'point_4' : [point_4],
-                'orientation' : orientation,
-                'text' : text
+                'file_name': file_name,
+                'image_width': image_width,
+                'image_height': image_height,
+                'category_id': category_id,
+                'points': [points],
+                'point_1': [point_1],
+                'point_2': [point_2],
+                'point_3': [point_3],
+                'point_4': [point_4],
+                'orientation': orientation,
+                'text': text
             }
             df_result = pd.concat([df_result, pd.DataFrame(temp_dict)])
-    
+
     return df_result
 
 
 def convert_to_dataframe(paths: List[str], pre_features: List[str]) -> pd.DataFrame:
-    
     df_infos = []
     for path in paths:
         json_info = _read_json(path)
